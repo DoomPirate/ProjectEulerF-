56d341877b7b30fe0deef54032c6d2ad27c17ea9
DoomPirate <nguyen.philip.h@gmail.com>
2022-08-05 07:43:50 -0400

    Update file.fs

--- file.fs
+++ file.fs
@@ -1,5 +1,43 @@
 
 (*
+Longest Collatz sequence
+Problem 14
+The following iterative sequence is defined for the set of positive integers:
+
+n → n/2 (n is even)
+n → 3n + 1 (n is odd)
+
+Using the rule above and starting with 13, we generate the following sequence:
+
+13 → 40 → 20 → 10 → 5 → 16 → 8 → 4 → 2 → 1
+It can be seen that this sequence (starting at 13 and finishing at 1) contains 10 terms. Although it has not been proved yet (Collatz Problem), it is thought that all starting numbers finish at 1.
+
+Which starting number, under one million, produces the longest chain?
+
+NOTE: Once the chain starts the terms are allowed to go above one million.
+*)
+
+// Few seconds?
+
+let collatz n = 
+    let rec collatz_inner (n:int64) (acc:int64): int64 = 
+        if n = 1L then 
+            acc + 1L
+        elif n % 2L = 0L then 
+            collatz_inner (n / 2L) (acc + 1L)
+        else     
+            collatz_inner (3L * n + 1L) (acc + 1L)
+
+    collatz_inner n 0L
+
+[1L..1_000_000L] 
+|> List.map(fun a -> (a, collatz a) )
+|> List.sortByDescending(fun b -> snd b)
+|> List.head
+|> fst
+|> printfn "%A"
+
+(*
 Work out the first ten digits of the sum of the following one-hundred 50-digit numbers.
 Split second
 *)
