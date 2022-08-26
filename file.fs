c7133149b89dab2d7c2a33863f150454de6cfd6c
DoomPirate <nguyen.philip.h@gmail.com>
2022-08-26 12:54:44 -0400

    Update file.fs

--- file.fs
+++ file.fs
@@ -1,3 +1,35 @@
+Factorial digit sum
+Submit
+
+ Show HTML problem content 
+Problem 20
+n! means n × (n − 1) × ... × 3 × 2 × 1
+
+For example, 10! = 10 × 9 × ... × 3 × 2 × 1 = 3628800,
+and the sum of the digits in the number 10! is 3 + 6 + 2 + 8 + 8 + 0 + 0 = 27.
+
+Find the sum of the digits in the number 100!
+let inline charToInt c = int c - int '0'
+
+let fact = 100
+
+(1,bigint 1 )
+|> Seq.unfold(fun (i,acc) ->
+                     if i <= fact - 1 then 
+                        let result = acc * (bigint i + bigint 1)
+                        Some(result,(i + 1,result))
+                     else 
+                        None) 
+|> Seq.last 
+|> string
+|> Seq.map charToInt
+|> Seq.reduce (+)
+|> printfn "%A"
+
+
+
+
+
 Largest product in a grid
 Submit
 
